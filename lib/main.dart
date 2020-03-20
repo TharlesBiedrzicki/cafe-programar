6cbb5e92f4b94f4a11b0bd58fa1cd47c4c80b1d8
TharlesBiedrzicki <tbiedrzicki@gmail.com>
2020-03-19 21:47:27 -0300

    implementação dos metodos

--- lib/main.dart
+++ lib/main.dart
@@ -15,7 +15,51 @@ class Inicial extends StatefulWidget {
 }
 
 class _InicialState extends State<Inicial> {
+  int cafe = 70 ;
+  int linhasCodigo = 0;
+  Color cor = Colors.green;
 
+  xicaraCafe(){
+    setState(() {
+      if(cafe >= 70){
+        cor = Colors.green;
+      }
+      if(cafe <= 69 && cafe >= 50){
+        cor = Colors.amber;
+      }
+      if(cafe <= 49 && cafe >= 25){
+        cor = Colors.orange;
+      }
+      if(cafe <= 24 && cafe >=0){
+        cor = Colors.red;
+      }
+    });
+  }
+
+  programar() {
+    if (cafe > 0) {
+      setState(() {
+          linhasCodigo ++;
+        cafe = cafe - 5;
+      });
+      xicaraCafe();
+    }
+  }
+  addCafe(){
+    if (cafe < 100) {
+      setState(() {
+        cafe = cafe + 5;
+      });
+      xicaraCafe();
+    }
+  }
+  reset(){
+    setState(() {
+      cafe = 70 ;
+      linhasCodigo = 0;
+      xicaraCafe();
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
